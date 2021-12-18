feaa228fa3d6ae83897e404cd71ab17e6d8423c5
Sean Warman <swarman@sysadmins-MacBook-Pro.local>
2021-12-18 05:28:10 +0000

    Removed socket.io code

--- index.js
+++ index.js
@@ -2,29 +2,27 @@ const express = require('express')
 const app = express()
 const http = require('http')
 const server = http.createServer(app)
-const { Server } = require('socket.io')
 const router = require('./router.js')
-const io = new Server(server)
 
 const port = 3000
 
 router(app)
 
-io.on('connection', (socket) => {
-  console.log('a user connected')
+// io.on('connection', (socket) => {
+//   console.log('a user connected')
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected')
+//   socket.on('disconnect', () => {
+//     console.log('user disconnected')
 
-  })
+//   })
 
-  socket.on('chat message', msg => {
-    console.log('message: ', msg)
+//   socket.on('chat message', msg => {
+//     console.log('message: ', msg)
 
-    io.emit('chat message', msg)
+//     io.emit('chat message', msg)
 
-  })
-})
+//   })
+// })
 
 
 server.listen(port, () => {
