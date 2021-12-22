6d3fc7337e4d9488b383445beb352a50858ca11e
Sean Warman <swarman@sysadmins-MacBook-Pro.local>
2021-12-22 11:03:44 +0000

    Allowed for nested dirs

--- router.js
+++ router.js
@@ -4,17 +4,21 @@ const readFile = promisify(fs.readFile)
 
 // This part can change to the dir you want to store your user's in.
 // For example on linux this could just be /home...
-const homedir = __dirname + '/app'
+const homeDir = __dirname + '/app/'
 
 async function response(req, res) {
-  const { params } = req
-  const { file: filename, username } = params
-  const path = `${homedir}/${username}`
-
   try {
-    const filepath = `${path}/${filename}`
-    const ext = filename.slice(filename.lastIndexOf('.'), -1)
-    const file = await readFile(filepath, 'utf8')
+    const { params } = req
+    const { userName } = params
+    const filePath = params[0]
+
+    if (!filePath) {
+      throw Error('No filePath param')
+    }
+
+    const path = `${homeDir}${userName}${filePath}`
+
+    const file = await readFile(path, 'utf8')
 
     res.send(file)
   } catch (e) {
@@ -24,7 +28,6 @@ async function response(req, res) {
   }
 }
 
-
 module.exports = function(app) {
-  app.get('/:username/:file', response)
+  app.get('/:userName*?', response)
 }
