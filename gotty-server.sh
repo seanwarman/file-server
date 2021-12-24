9252988a8c04cdfa7f087c75aded603364c9d848
root <root@cd3k-server-1.localdomain>
2021-12-24 07:37:03 +0000

    Added .gotty config and removed un-needed copy commds

--- gotty-server.sh
+++ gotty-server.sh
@@ -4,8 +4,8 @@
 docker build -t file-server .
 
 # Runs gotty ready for the arguments to log in as a specific user...
-./gotty -w -p 3005 --permit-arguments docker exec -it -u
+./gotty -w -p 8080 --config .gotty --permit-arguments docker exec -it -u
 
 # Example:
-# http://myserve.org:3005/?arg=sean&arg=sean-server&arg=bash
+# http://myserve.org:8080/?arg=sean&arg=sean-server&arg=bash
 #                              ^user    ^container      ^exec command
