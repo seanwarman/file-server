658f3a24c8a5e93ab9e24785473032ec46e28fe6
vps-cd3k <seanwarman@gmail.com>
2021-12-29 20:42:18 +0000

    Added ssl password to useradd script

--- docker-useradd.sh
+++ docker-useradd.sh
@@ -6,5 +6,6 @@
 mkdir home/$1
 # Allow access for all users, so they can make changes
 chmod -R 757 home/$1
+
 # Run a new container and create the new user inside it...
-docker run -d -t -v "$(pwd)"/home/$1:/home/$1 --name $1-server -w /home/$1 file-server bash -c "useradd -m $1 && bash"
+docker run -d -t -v "$(pwd)"/home/$1:/home/$1 --name $1-server -w /home/$1 file-server bash -c "useradd -p \$(openssl passwd -1 $2) $1 && bash"
