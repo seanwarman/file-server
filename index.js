9952cba37d83c102fd2e38e1291ad640007b6d09
vps-cd3k <seanwarman@gmail.com>
2022-01-01 20:38:57 +0000

    Added markdown rendering

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
 const express = require('express')
 const http = require('http')
 const router = require('./router.js')
+const marked = require('marked')
+const fs = require('fs')
+const promisify = require('util').promisify
+const readFile = promisify(fs.readFile)
 
 const app = express()
 app.set('views', './home')
+app.engine('md', async (filePath, options, cb) => {
+  const file = await readFile(filePath, 'utf8')
+
+  return cb(null, marked.parse(file))
+})
 
 const server = http.createServer(app)
 
 const port = process.argv[2] || 3000
 
 router(app)
-
-// io.on('connection', (socket) => {
-//   console.log('a user connected')
-
-//   socket.on('disconnect', () => {
-//     console.log('user disconnected')
-
-//   })
-
-//   socket.on('chat message', msg => {
-//     console.log('message: ', msg)
-
-//     io.emit('chat message', msg)
-
-//   })
-// })
-
 
 server.listen(port, () => {
   console.log(`listening on ${port}`)
