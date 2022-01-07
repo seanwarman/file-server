974e398dac2ea73c95d4d78518398015da88cf25
root <root@cd3k-server-1.localdomain>
2022-01-07 05:25:01 +0000

    Removed port from gotty server script

--- gotty-server.sh
+++ gotty-server.sh
@@ -3,16 +3,10 @@
 
 docker build -t file-server .
 
-if [-z $2]; then
-  port=8080
-else
-  port=$2
-fi
-
 # Runs gotty ready for the arguments to log in as a specific user.
 # The first arg for this script is a user:password combination...
-./gotty -w -p $port -c $1 --config .gotty --permit-arguments docker exec -it -u || \
-gotty -w -p $port -c $1 --config .gotty --permit-arguments docker exec -it -u
+./gotty -w -p 8080 -c $1 --config .gotty --permit-arguments docker exec -it -u || \
+gotty -w -p 8080 -c $1 --config .gotty --permit-arguments docker exec -it -u
 
 # Example:
 # http://myserve.org:8080/?arg=sean&arg=sean-server&arg=bash
