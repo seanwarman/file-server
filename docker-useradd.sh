843f734826f890cd8dbe4e662ecc87f24acdfe36
Sean Warman <seanwarman@gmail.com>
2023-01-10 22:39:15 +0000

    Image includes sudo

--- docker-useradd.sh
+++ docker-useradd.sh
@@ -8,4 +8,4 @@ mkdir home/$1
 chmod -R 757 home/$1
 
 # Run a new container and create the new user inside it...
-docker run -d -t -v "$(pwd)"/home/$1:/home/$1 --name $1-server -w /home/$1 file-server bash -c "useradd -p \$(openssl passwd -1 $2) $1 && bash"
+docker run -d -t -v "$(pwd)"/home/$1:/home/$1 --name $1-server -w /home/$1 file-server bash -c "useradd -p \$(openssl passwd -1 $2) $1 && usermod -aG sudo $1 && bash"
